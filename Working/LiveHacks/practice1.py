54c1dbc7643c6f2d8856d6f6ab43a28ce353ba23
123andrewjiang <andrew.jiang22@ycdsbk12.ca>
2019-10-01 09:47:49 -0400

    practice

---
+++ Working/LiveHacks/practice1.py
@@ -0,0 +1,19 @@
+'''
+_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+Name:         
+
+
+
+
+
+
+
+
+# get fahrenheit from user
+fahrenheit = float(input("Enter the fahrenheit"))
+
+# compute celsius from fahrenheit
+celsius = 5/9 * (fahrenheit - 32)
+
+# output celsius
+print("the temperature in celsius" + str(celsius))
